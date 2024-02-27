9634f365b2417e25f20d1e003dfc73a6c9523a7e
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-02-27 13:55:20 -0800

    added tags

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,37 @@ import './App.css'
 import { css } from '@emotion/react';
 
 function App() {
-  const [filterOptions, setFilterOptions] = useState([])
-  const [filterForm, setFilterForm] = useState([])
-  const [results, setResults] = useState([])
+  const [filterOptions, setFilterOptions] = useState([]);
+  const [tagOptions, setTagOptions] = useState([]);
+  const [filterForm, setFilterForm] = useState([]);
+  const [tagForm, setTagForm] = useState([]);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     let ingredients = [];
+    let tags = [];
 
     foodData.map(meal => {
       for (let i = 0; i < meal.ingredients.length; i++) {
-        if (!ingredients.find(item => item === meal.ingredients[i])) {
+        if (!ingredients.find(ingredient => ingredient === meal.ingredients[i])) {
           ingredients.push(meal.ingredients[i]);
+        }
+      }
+      for (let j = 0; j < meal.tags.length; j++) {
+        if (!tags.find(tag => tag === meal.tags[j])) {
+          tags.push(meal.tags[j]);
         }
       }
     })
 
     setFilterOptions(ingredients);
+    setTagOptions(tags);
   }, [foodData])
 
   // remove!!
   useEffect(() => {
-    console.log(filterForm);
-  }, [filterForm])
+    console.log(tagForm);
+  }, [tagForm])
 
   const addFilter = (event) => {
     if (event.target.checked) {
@@ -34,6 +43,16 @@ function App() {
     else {
       let unselect = filterForm.filter(ingredient => ingredient !== event.target.id);
       setFilterForm(unselect);
+    }
+  }
+  
+  const addTag = (event) => {
+    if (event.target.checked) {
+      setTagForm(pre => [...pre, event.target.id])
+    }
+    else {
+      let unselect = tagForm.filter(tag => tag !== event.target.id);
+      setTagForm(unselect);
     }
   }
 
@@ -47,9 +66,8 @@ function App() {
         <h1>What's for Dinner?</h1>
       </header>
       <main>
-        <h2>Select Ingredients Not on Hand</h2>
+        <h2>Select All Ingredients Not on Hand</h2>
 
-        <form>
           <div id="filters">
             {filterOptions.length ? (
               filterOptions.map((ingredient, index) =>
@@ -60,8 +78,19 @@ function App() {
               )
             ) : null}
           </div>
+
+          <h2>Include Only:</h2>
+          <div id="options">
+              {tagOptions.length ? (
+                tagOptions.map((tag, index) =>
+                  <div id="tag-container" key={index}>
+                    <input id={tag} type="checkbox" value={tag} onClick={addTag}></input>
+                    <label id="tag-label" htmlFor={tag}>{tag}</label>
+                  </div>
+                )
+              ): null}
+          </div>
           <button onClick={handleChoice}>What's for Dinner?</button>
-        </form>
 
         <div id="results">
           <div id="image-container">
@@ -70,6 +99,9 @@ function App() {
         </div>
 
       </main>
+      <footer>
+        <p>Eh who needs a footer</p>
+      </footer>
     </div>
   )
 }
