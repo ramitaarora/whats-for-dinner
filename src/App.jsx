fd45dc663c07c5f894bd6900d992bc1741818d81
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-02-27 13:44:22 -0800

    filtering by ingredients

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import foodData from './foodData.json';
 import './App.css'
+import { css } from '@emotion/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [filterOptions, setFilterOptions] = useState([])
+  const [filterForm, setFilterForm] = useState([])
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    let ingredients = [];
+
+    foodData.map(meal => {
+      for (let i = 0; i < meal.ingredients.length; i++) {
+        if (!ingredients.find(item => item === meal.ingredients[i])) {
+          ingredients.push(meal.ingredients[i]);
+        }
+      }
+    })
+
+    setFilterOptions(ingredients);
+  }, [foodData])
+
+  // remove!!
+  useEffect(() => {
+    console.log(filterForm);
+  }, [filterForm])
+
+  const addFilter = (event) => {
+    if (event.target.checked) {
+      setFilterForm(pre => [...pre, event.target.id])
+    }
+    else {
+      let unselect = filterForm.filter(ingredient => ingredient !== event.target.id);
+      setFilterForm(unselect);
+    }
+  }
+
+  const handleChoice = (event) => {
+    event.preventDefault();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <header>
+        <h1>What's for Dinner?</h1>
+      </header>
+      <main>
+        <h2>Select Ingredients Not on Hand</h2>
+
+        <form>
+          <div id="filters">
+            {filterOptions.length ? (
+              filterOptions.map((ingredient, index) =>
+                <div id="filter-container" key={index}>
+                  <input id={ingredient} type="checkbox" value={ingredient} onClick={addFilter}></input>
+                  <label id="filter-label" htmlFor={ingredient}>{ingredient}</label>
+                </div>
+              )
+            ) : null}
+          </div>
+          <button onClick={handleChoice}>What's for Dinner?</button>
+        </form>
+
+        <div id="results">
+          <div id="image-container">
+              <img src="" alt=""/>
+          </div>
+        </div>
+
+      </main>
+    </div>
   )
 }
 
-export default App
+export default App;
